7c0b1da95fb6c3d2ba98c5c5bd81cf927c76241d
Joey Farina <jrf61194@gmail.com>
2016-12-08 18:57:39 -0500

    spotify access token setup

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>It Works!</h1>
+        <p>Enjoy!</p>
+      </div>
+    )
+  }
+}
