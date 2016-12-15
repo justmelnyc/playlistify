338edd0ceaff3330c8a27d673dee40425ff16e8e
Joey Farina <jrf61194@gmail.com>
2016-12-14 23:36:18 -0500

    App refactor

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -2,39 +2,26 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import Viewer from './Viewer';
-import Sorter from './Sorter';
 import { login } from './../actions/login'
-import ProfileBar from './../components/ProfileBar';
 
-import Table from './../components/Table';
 
 const propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  user: PropTypes.object.isRequired,
-  musicData: PropTypes.object.isRequired
+  dispatch: PropTypes.func.isRequired
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
 
   componentDidMount() {
     const { dispatch } = this.props
     dispatch(login())
   }
 
-  headerClickHandler() {
-    console.log('header')
-  }
-
   render() {
-    const { musicData, songs, user } = this.props
-    const { profile } = user
-
-    console.log(user)
-
-    console.log(musicData)
     return (
       <div className="Container" >
-        <ProfileBar profile={profile} />
         <Viewer />
       </div>
     )
@@ -44,10 +31,7 @@ class App extends Component {
 App.propTypes = propTypes;
 
 function mapStateToProps(state) {
-  return {
-    user: state.user,
-    musicData: state.musicData
-  }
+  return {}
 }
 
 export default connect(mapStateToProps)(App);
