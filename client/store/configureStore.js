139b24aa9de32e10e8e7e81aecbae1a5ba8928ba
Joey Farina <jrf61194@gmail.com>
2016-12-16 11:46:50 -0500

    composeWithDevtools added

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import rootReducer from './../reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const loggerMiddleware = createLogger()
 
@@ -9,9 +10,11 @@ export default function configureStore(preloadedState) {
   return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
+    composeWithDevTools(
+      applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+      )
     )
   )
 }
