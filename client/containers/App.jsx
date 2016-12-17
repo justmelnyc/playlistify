e9e47945cf1ab743323937630bb17e4b809376b4
Joey Farina <jrf61194@gmail.com>
2016-12-17 16:15:33 -0500

    loader css

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -42,21 +42,30 @@ class App extends Component {
         <Filters />
         <Viewer />
 
-        /*
-          TODO:
-          Refactor This Loading screen
-         */
-
         {isLoggedIn && profile === null &&
-          <div className='Loader'>LOADING PROFILE</div>
+          <div className='Loader'>
+            LOADING PROFILE
+          <svg className='spinner' width='65px' height='65px' viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
+            <circle className='path' fill='none' strokeWidth='6' strokeLinecap='round' cx='33' cy='33' r='30' />
+          </svg>
+          </div>
+
         }
 
         {isLoggedIn && profile && !trackList.length &&
-          <div className='Loader'> LOADING TRACKS can take a while</div>
+          <div className='Loader'>
+
+          LOADING TRACKS can take a while
+           <svg className='spinner' width='65px' height='65px' viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
+             <circle className='path' fill='none' strokeWidth='6' strokeLinecap='round' cx='33' cy='33' r='30' />
+           </svg></div>
         }
 
         {creatingPlaylist &&
-          <div className='Loader'>Generating the playlist {playlistName} for you </div>
+          <div className='Loader'>Generating the playlist {playlistName} for you
+           <svg className='spinner' width='65px' height='65px' viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>
+             <circle className='path' fill='none' strokeWidth='6' strokeLinecap='round' cx='33' cy='33' r='30' />
+           </svg></div>
         }
 
       </div>
