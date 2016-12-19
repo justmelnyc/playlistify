6ad6d42eec6d9813c71802baf41923324cf03b9b
Joey Farina <jrf61194@gmail.com>
2016-12-19 16:26:29 -0500

    bin/www

--- server.js
+++ server.js
@@ -139,4 +139,27 @@ app.get('*', function (req, res) {
 })
 
 console.log('Listening on 3000')
-app.listen(3000)
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+module.exports = app
